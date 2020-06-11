c24d72a7c22e3c3e9cd83faf68ef954586f1170c
goman_iv <realtt@mail.ru>
2020-06-11 13:00:16 +0300

    Add Contact class

---
+++ SolutionXPO/SolutionXPO.Module/BusinessObjects/Contacts.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using DevExpress.Xpo;
+using DevExpress.ExpressApp;
+using System.ComponentModel;
+using DevExpress.ExpressApp.DC;
+using DevExpress.Data.Filtering;
+using DevExpress.Persistent.Base;
+using System.Collections.Generic;
+using DevExpress.ExpressApp.Model;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.Validation;
+
+namespace SolutionXPO.Module.BusinessObjects
+{
+    [DefaultClassOptions]
+    public class Contact : Person
+    {
+        public Contact(Session session) : base(session) { }
+        private string webPageAddress;
+        public string WebPageAddress
+        {
+            get { return webPageAddress; }
+            set { SetPropertyValue(nameof(WebPageAddress), ref webPageAddress, value); }
+        }
+        private string nickName;
+        public string NickName
+        {
+            get { return nickName; }
+            set { SetPropertyValue(nameof(NickName), ref nickName, value); }
+        }
+        private string spouseName;
+        public string SpouseName
+        {
+            get { return spouseName; }
+            set { SetPropertyValue(nameof(SpouseName), ref spouseName, value); }
+        }
+        private TitleOfCourtesy titleOfCourtesy;
+        public TitleOfCourtesy TitleOfCourtesy
+        {
+            get { return titleOfCourtesy; }
+            set { SetPropertyValue(nameof(TitleOfCourtesy), ref titleOfCourtesy, value); }
+        }
+        private DateTime anniversary;
+        public DateTime Anniversary
+        {
+            get { return anniversary; }
+            set { SetPropertyValue(nameof(Anniversary), ref anniversary, value); }
+        }
+        private string notes;
+        [Size(4096)]
+        public string Notes
+        {
+            get { return notes; }
+            set { SetPropertyValue(nameof(Notes), ref notes, value); }
+        }
+    }
+    public enum TitleOfCourtesy { Dr, Miss, Mr, Mrs, Ms };
+}
