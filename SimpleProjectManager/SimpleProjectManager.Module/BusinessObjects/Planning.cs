87e49da7dbadc3a709882fc1504a7d1293731668
NNIIRT\goman_iv <yuy6^YUY>
2020-06-11 09:29:59 +0300

    Add conditionalAppearance

--- SimpleProjectManager/SimpleProjectManager.Module/BusinessObjects/Planning.cs
+++ SimpleProjectManager/SimpleProjectManager.Module/BusinessObjects/Planning.cs
@@ -2,10 +2,22 @@
 using DevExpress.Persistent.Base;
 using DevExpress.Persistent.BaseImpl;
 using DevExpress.Xpo;
+using DevExpress.Persistent.Validation;
+using DevExpress.ExpressApp.ConditionalAppearance;
+using System.Drawing;
 
 namespace SimpleProjectManager.Module.BusinessObjects.Planning
 {
     [NavigationItem("Planning")]
+    [Appearance("Completed1", TargetItems = "Subject",
+    Criteria = "Status = 'Completed'", FontStyle = FontStyle.Strikeout, FontColor = "ForestGreen")]
+    [Appearance("Completed2", TargetItems = "*;Status;AssignedTo",
+    Criteria = "Status = 'Completed'", Enabled = false)]
+    [Appearance("InProgress", TargetItems = "Subject;AssignedTo",
+    Criteria = "Status = 'InProgress'", BackColor = "LemonChiffon")]
+    [Appearance("Deferred", TargetItems = "Subject",
+    Criteria = "Status = 'Deferred'", BackColor = "MistyRose")]
+    [RuleCriteria("EndDate >= StartDate")]
     public class ProjectTask : BaseObject
     {
         public ProjectTask(Session session) : base(session) { }
